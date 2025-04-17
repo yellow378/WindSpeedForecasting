aa9bc935520bb1ac92f6d08e363e4948d0f9efbf
yellow378 <924697207@qq.com>
2025-04-18 00:45:23 +0800

    训练脚本

---
+++ myScripts/SparseTSF.sh
@@ -0,0 +1,39 @@
+#SparseTSF
+files=(51)
+# 遍历文件列表
+for file in "${files[@]}"; do
+  # 构造文件路径
+  data_path="Turb${file}.csv"
+  model_id="Wind_SparseTSF_432_36_Turb${file}"
+  # 打印当前处理的文件
+  echo "Processing file: $data_path"
+  echo "Model id: ${model_id}"
+  # 运行 Python 脚本
+  cmd="python -u run.py --is_training 1 --root_path ./dataset/processed \
+      --data_path ${data_path} --model_id ${model_id} --model SparseTSF --task_name long_term_forecast \
+      --data custom --features MS --target Wspd --seq_len 432 --label_len 18 --pred_len 36 --enc_in 10 --dec_in 10 \
+      --c_out 1 --dropout 0.1 --train_epochs 40 --patience 10 --loss mse --use_gpu True --inverse --model_type mlp \
+      --d_model 128 --period_len 9 --itr 1 --batch_size 256 --learning_rate 0.001"
+
+  # 定义日志文件路径
+  log_file="logs/LongForecasting/SparseTSF/${model_id}.log"
+  # 如果日志文件存在，则删除它
+  if [ -f "$log_file" ]; then
+    rm "$log_file"
+    echo "Removed existing log file: $log_file"
+  fi
+
+  # 打印分隔符、命令以及再次打印分隔符到日志文件
+  echo "========================================================================" >>$log_file
+  echo "Executing command at $(date):" >>$log_file
+  echo "$cmd" >>$log_file
+  echo "------------------------------------------------------------------------" >>$log_file
+
+  # 执行命令并将输出追加到日志文件
+  $cmd >>$log_file 2>&1
+
+  # 在日志文件中记录命令结束的时间戳
+  echo "Command finished at $(date)" >>$log_file
+  echo "========================================================================" >>$log_file
+done
+echo "All files processed successfully!"
